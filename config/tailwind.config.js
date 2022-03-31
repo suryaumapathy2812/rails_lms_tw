2e0a73dd212c227c223f740f632a73f3444b72cd
Surya Umapathy <suryaumapathy2812@gmail.com>
2022-03-31 18:43:41 +0530

    Fix: course show html current_user

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -11,12 +11,19 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      lineClamp: {
+        10: '10',
+        14: '14',
+        16: '16',
+        20: '10',
+      }
     },
   },
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography'),
-    require('tailwindcss/colors')
+    require('tailwindcss/colors'),
+    require('@tailwindcss/line-clamp'),
   ]
 }
