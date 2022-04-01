3c48ae166f6707fe8910305b32fcb8e061123a57
Surya Umapathy <suryaumapathy2812@gmail.com>
2022-04-01 17:32:41 +0530

    Feat: home page added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
+  layout "application"
+
 
   def index
-    render 'index'
+    @courses = Course.all
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #
 
-  root 'course#index'
+  root 'home#index'
 
   # Courses Routes
   get '/courses', to: 'course#index', as: :courses
